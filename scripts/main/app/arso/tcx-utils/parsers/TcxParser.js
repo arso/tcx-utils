58a1503185d00ac40c6eb38afea8d97ecca039bf
Artur Socha <artur.socha.mailbox@gmail.com>
2013-08-25 20:34:14 +0200

    longitude & latitude extracted from tcx

--- scripts/main/app/arso/tcx-utils/parsers/TcxParser.js
+++ scripts/main/app/arso/tcx-utils/parsers/TcxParser.js
@@ -1,14 +1,21 @@
-define(function () {
+define(['jquery'], function ($) {
 
     return {
+
         getName: function () {
             return 'tcxParser';
         },
-        extractGeoTrackPoints: function (tcxXml) {
-            //todo use xpath to select all Trackpoint/Position/{LatitudeDegrees|LongitudeDegrees}
-            console.log('extract_geo_tractpoints not yet implememted !!');
-            var trackPoints = new Array();
 
+        extractGeoTrackPoints: function (tcxXml) {
+            var trackPoints = new Array();
+            $(tcxXml).find('Position').each(function () {
+                var lattitude = $(this).find('LatitudeDegrees').text();
+                var longitude = $(this).find('LongitudeDegrees').text();
+                //console.log(lattitude +", "+longitude);
+                var point = {latitude: lattitude, longitude: longitude};
+                trackPoints.push(point);
+            })
+            console.log("Found " + trackPoints.length + " geo points.");
             return trackPoints;
         }
     };
